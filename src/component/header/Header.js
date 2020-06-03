f8f45ed6968739493790b7276c62195c0d000844
Viktor <nayutalienx@gmail.com>
2020-06-03 23:34:58 +0300

    Частичто переделал шапку с целью переезда на адаптив

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './Header.css'
-import logo from '../../logo.svg';
+import logo from '../../camellialogo.png';
+import userPhoto from '../../user.jpg'
 import "bootstrap/dist/css/bootstrap.min.css"
 import {Link} from "react-router-dom";
 
@@ -17,25 +18,29 @@ class Header extends React.Component {
     render() {
 
         return (
-            <header className="d-flex justify-content-around header">
-                <img src={logo} className="header-logo"/>
+            <div className="d-flex justify-content-between" id="header">
 
-                <div className="header-item align-self-center">
-                    <Link to="/home">Основная</Link>
+
+                <div className="header-item" id="logo-text">
+                    <img src={logo} style={{
+                        "width": "100%",
+                        "height": "auto",
+                        "max-width": "150px",
+                        "margin-left": "10px"
+                    }}/>
                 </div>
 
-                <input className="flex-fill ml-2 mr-2 header-search" placeholder="Поиск ..."/>
-
-                <img src={logo} className="header-logo"/>
-                <div className="header-item align-self-center dropdown">
-                    Мой профиль
-                    <div className="dropdown-content">
-                        <div className="dropdown-content-item"><Link to="/me">Мои игры</Link></div>
-                        <div className="dropdown-content-item">Настройки</div>
-                        <div className="dropdown-content-item">Выход</div>
-                    </div>
+                <div className="header-item w-50">
+                    <input type="text" id="search-bar" placeholder="Поиск"/>
                 </div>
-            </header>
+
+                <div className="header-item" id="header-profile-bar">
+
+                    <img src={userPhoto} id="user-photo"/>
+                    <div id="profile-link">Мой профиль</div>
+
+                </div>
+            </div>
         );
     }
 
