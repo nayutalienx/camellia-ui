0bc67ca5c61a67a0c98485031e11f850091c7d08
Viktor <nayutalienx@gmail.com>
2020-06-07 00:02:41 +0300

    Updates

---
+++ precache-manifest.ceddebb35fea136eb5cd5ac0bd1e0907.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63eaeb6e3ca6054912e587c89c58b50e",
+    "url": "/camellia-ui/index.html"
+  },
+  {
+    "revision": "56ae94935ac62d8c719d",
+    "url": "/camellia-ui/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f57f2aede61ddb8d33f7",
+    "url": "/camellia-ui/static/css/main.f901ae84.chunk.css"
+  },
+  {
+    "revision": "56ae94935ac62d8c719d",
+    "url": "/camellia-ui/static/js/2.6ec1ae94.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/camellia-ui/static/js/2.6ec1ae94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f57f2aede61ddb8d33f7",
+    "url": "/camellia-ui/static/js/main.27038e74.chunk.js"
+  },
+  {
+    "revision": "fc5d8c463f1ca971adf4",
+    "url": "/camellia-ui/static/js/runtime-main.2b5fd897.js"
+  },
+  {
+    "revision": "22e3b4e78059f8396c5b25fb9efc324e",
+    "url": "/camellia-ui/static/media/user.22e3b4e7.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.508fb20e33bc1b5b5876ff6e0f281340.js"
+  "/camellia-ui/precache-manifest.ceddebb35fea136eb5cd5ac0bd1e0907.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/camellia-ui/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
