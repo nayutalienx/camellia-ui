f3b5331cf4c10122e18aca11d81f06225a94a67f
nayutalienx <47031833+nayutalienx@users.noreply.github.com>
2020-06-09 22:06:59 +0300

    Добавил компонент игры

---
+++ src/component/game/Game.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import './Game.css'
+
+
+class Game extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                THIS IS GAME PAGE!
+            </div>
+        )
+    }
+
+
+}
+
+export default Game
